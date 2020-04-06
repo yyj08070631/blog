29874dd06959239ddb4666171019bf9e653430d7
YYJ <934780820@qq.com>
2020-04-06 11:54:18 +0800

    update build code

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yyj08070631/yyj.git master:gh-pages
+git push -f "https://${GITHUB_TOKEN}@${GITHUB_REF}" master:gh-pages
 
 cd -
