39b97f77e1bf2e583d1d76f88aaad52ed7057548
YYJ <934780820@qq.com>
2020-06-04 23:14:52 +0800

    add baidu tongji

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,12 +1,3 @@
-// start baidu tongji
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?34ffe82902ef2e02b9f8f8642ace8775";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
-
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
@@ -15,4 +6,14 @@ export default ({
   isServer // 当前应用配置是处于 服务端渲染 或 客户端
 }) => {
   // ...做一些其他的应用级别的优化
+  if (!isServer) {
+    // start baidu tongji
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?34ffe82902ef2e02b9f8f8642ace8775";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(hm, s);
+    })();
+  }
 }
